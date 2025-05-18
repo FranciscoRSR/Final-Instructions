eea7b36305d71335e679fc5f441cb2348b606ca8
FranciscoRSR <francisco@rsrnurburg.com>
2025-05-18 12:03:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1900,6 +1900,7 @@ function generatePDFContent(instruction, trackDetails) {
         grid-template-columns: 48% 48%;
         column-gap: 2%;
         width: 100%;
+        height: 100%;
         padding: 0;
         margin: 0;
       }
